e95c33afa9bdf4e2fa3affe835875b6634ec5f44
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-12-01 23:11:32 +0300

    Сreated a class to collect information about classes.

---
+++ TestsGeneratorLibrary/ClassInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace TestsGeneratorLibrary
+{
+    public class ClassInfo
+    {
+        public string Name { get; set; }
+        public string NamespaceName { get; set; }
+        public List<string> MethodNames { get; set; }
+
+        public ClassInfo() { }
+
+        public ClassInfo(string name, string namespaceName, List<string> methodNames)
+        {
+            Name = name;
+            NamespaceName = namespaceName;
+            MethodNames = methodNames;
+        }
+    }
+}
