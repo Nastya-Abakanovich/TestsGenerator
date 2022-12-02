1c796fd0e492362c446e2de68ae45f52e20ac7dc
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-12-03 01:07:16 +0300

    Added example of use.

---
+++ Example/TestClasses/Class1.cs
@@ -0,0 +1,31 @@
+﻿
+namespace TestClasses
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+
+        }
+
+        public int Method2()
+        {
+            return 0;
+        }
+
+    }
+
+    public class Class2
+    {
+        public void Method()
+        {
+
+        }
+
+        public void Method(int x)
+        {
+
+        }
+
+    }
+}
---
+++ Example/TestClasses/Class3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClasses
+{
+    public class Class3
+    {
+        private void PrivateMethod()
+        {
+
+        }
+        public void Method1()
+        {
+
+        }
+
+        public int Method2(int x, int y)
+        {
+            return 0;
+        }
+
+
+    }
+}
