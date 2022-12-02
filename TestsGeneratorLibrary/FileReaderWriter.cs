5e877d84533de026f4f2f4842ac82dd1111ef9ff
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-12-03 00:47:14 +0300

    Created class for working with files.

---
+++ TestsGeneratorLibrary/FileReaderWriter.cs
@@ -0,0 +1,33 @@
+﻿namespace TestsGeneratorLibrary
+{
+    public class FileReaderWriter
+    {
+        public async Task<string> Read(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                throw new ArgumentException("File doesn't exists.");
+            }
+
+            String result = "";
+            using (var reader = File.OpenText(filename))
+            {
+                result = await reader.ReadToEndAsync();                
+            }
+                
+            return result;
+        }
+
+        public async Task Write(string filePath, string fileContent)
+        {
+            if (!Directory.Exists(filePath))
+            {
+                throw new ArgumentException("Directory doesn't exists.");
+            }
+
+            await using var writer = new StreamWriter(filePath);
+            await writer.WriteAsync(fileContent);
+        }
+
+    }
+}
